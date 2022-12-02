97e63863d8ee3402f80e45f2012e286dd827f632
root <root@ip-172-31-87-112.ec2.internal>
2022-12-02 19:43:24 +0000

    do git_add_commit_push_Sync(undefined).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -2,7 +2,7 @@ var e_Note_json =
 {
     "Gen": {
         "1": {
-            "1": "p002,p010,wdingpub01,guest001,guest,xy11,xy10,xy12,gm001"
+            "1": "pub01,p002,p010,wdingpub01,guest001,guest,xy11,xy10,xy12,gm001"
         }
     }
 }
