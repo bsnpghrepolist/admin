cfbc74a6017064cc2bea08869875199c17332c66
root <root@ip-172-31-87-112.ec2.internal>
2022-12-02 22:19:56 +0000

    do git_add_commit_push_Sync(admin add usr).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -3,7 +3,8 @@ var e_Note_json =
     "Gen": {
         "1": {
             "1": {
-                "pub01": 1
+                "pub01": 1,
+                "wdingpub01": 1
             },
             "4": {
                 "pub01": 1
