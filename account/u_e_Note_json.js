2f5d28efb81496544dd348f43b37cfdcc6a2df6d
root <root@ip-172-31-87-112.ec2.internal>
2023-03-19 07:37:10 +0000

    do git_add_commit_push_Sync(admin add usr).

--- account/u_e_Note_json.js
+++ account/u_e_Note_json.js
@@ -235,6 +235,9 @@ var u_e_Note_json =
         "26": {
             "1": {
                 "wdingpba": 1
+            },
+            "3": {
+                "wdingpba": 1
             }
         },
         "31": {
